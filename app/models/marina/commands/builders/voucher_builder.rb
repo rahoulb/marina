a58e23a31b61d902aea7417efd36cbfb9ee87bf1
cmthakur <cmthakur.r2s@gmail.com>
2014-02-18 20:15:41 +0545

    Add command voucher builder

---
+++ app/models/marina/commands/builders/voucher_builder.rb
@@ -0,0 +1,19 @@
+require_relative '../builder'
+
+module Marina
+  module Commands
+    module Builders
+      class VoucherBuilder < Builder
+
+        def initialize params = {}
+          super params.merge(permission: :manage_vouchers)
+        end
+
+        def do_create params = {}
+
+        end
+
+      end
+    end
+  end
+end
