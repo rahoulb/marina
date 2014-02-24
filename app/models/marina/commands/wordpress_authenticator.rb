8c6f9dfe7a52bc98c5fa71516393dede3003c1ae
Rahoul Baruah <rahoul@3hv.co.uk>
2014-02-24 16:29:40 +0000

    bugfix on authenticator

--- app/models/marina/commands/wordpress_authenticator.rb
+++ app/models/marina/commands/wordpress_authenticator.rb
@@ -3,10 +3,10 @@ module Marina
   module Commands
     class WordpressAuthenticator
       def user_id_from request, session, cookies
-        Marina::Db::Member.by_username(username_from(cookies))
+        Marina::Db::Member.by_username(username_from(request, cookies))
       end
 
-      def username_from cookies
+      def username_from request, cookies
         site_name = "#{request.protocol}#{request.host_with_port}"
         hash = Digest::MD5.hexdigest site_name
         value = cookies["wordpress_logged_in_#{hash}"].to_s.split('|').first
