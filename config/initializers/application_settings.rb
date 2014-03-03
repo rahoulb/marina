b94d02ab6ebeb8fa4997b9e4a241cb0daafb705c
Rahoul Baruah <rahoul@3hv.co.uk>
2014-03-03 22:22:06 +0000

    Added framework for configurable processors

--- config/initializers/application_settings.rb
+++ config/initializers/application_settings.rb
@@ -1,3 +1,5 @@
+# Initializers are loaded in alphabetical order, so application_settings should load first - subsequent initializers can then read the values from Marina::Application.config
+
 raw_config = YAML.load File.open("#{Rails.root}/config/application.yml")
 config = raw_config[Rails.env]
 
--- config/initializers/payment_processor.rb
+++ config/initializers/payment_processor.rb
@@ -1,1 +1,1 @@
-Rails.application.config.payment_processor = nil
+Rails.application.config.payment_processor = Marina::Application.config.payment_processor_class.constantize.new
